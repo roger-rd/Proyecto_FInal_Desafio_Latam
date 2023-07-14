69c3375a05b9ada5c2a7335cdd7b17739e05e6aa
Roger Rodriguez Pire <rogerrodriguez@MacBook-Pro-de-Roger.local>
2023-07-14 18:54:21 -0400

    se modifica la pagina main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 
 import UserContextProvider from "./context/UserContext"
 import OperationsContextProvider from "./context/OperationsContext";
+import PerfilProvider from './context/PerfilContext';
 
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext';
 
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
   <React.StrictMode>
-  <BrowserRouter>
-    <UserContextProvider >
-      <OperationsContextProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </OperationsContextProvider>
-    </UserContextProvider>
-  </BrowserRouter>
-</React.StrictMode>
+    <BrowserRouter>
+      <UserContextProvider>
+        <PerfilProvider>
+          <OperationsContextProvider>
+            <AuthProvider>
+              <App />
+            </AuthProvider>
+          </OperationsContextProvider>
+        </PerfilProvider>
+      </UserContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
+
--- src/view/Perfil.jsx
+++ src/view/Perfil.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useContext,  } from "react";
+import React, { useEffect, useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import { perfilContext } from "../context/PerfilContext";
 
 export default function Perfil() {
   const { setUsuario } = useContext(UserContext);
-  const { setUsuarios} = useContext(perfilContext);
+  const { usuarios } = useContext(perfilContext); // Corregir la destructuración para obtener 'usuarios'
 
   useEffect(() => {
     const usuarioLocalStorage = localStorage.getItem("usuario");
@@ -14,42 +14,129 @@ export default function Perfil() {
     }
   }, []);
 
-
   return (
-    <><div>
-      <h1> <span className="badge bg-secondary">Bienvenid@</span></h1>
-    </div><div class="container text-center">
+    <>
+      <div>
+        <h1>
+          <span className="badge bg-secondary">Bienvenid@</span>
+        </h1>
+      </div>
+      <div class="container text-center">
         <div class="row">
-        <div >
-         {/* <Carousel/> */}
-                <div className="perfil" >
-                    {usuarios.map(item =>(
-                        <div className="row-cols-1 row-cols-md-1 " key={item.id} >
-                            <div className="col">
-                                <div className="card h-100">                          
-                                    <div className="card-body">
-                                        <h5 className="card-title">{item.nombre}</h5>
-                                    </div>
-                                    <hr />
-                                    <div className="card-footer" >
-                                        <h5 className="text-muted">$ {item.correo}</h5>                              
-                                    </div>
-                                    <div className="buttonHome">
-                                        <button className="btn btn-primary" onClick={() => {  
-                                            navigate(`/Favoritos/${item.id}`);
-                                        }}>Favoritos 👀</button>
-                                      
-                                    </div>
-                                </div>
-                            </div>
+          <div>
+            {/* <Carousel/> */}
+            <div className="perfil">
+              {usuarios &&
+                usuarios.map((item) => (
+                  <div className="row-cols-1 row-cols-md-1 " key={item.id}>
+                    <div className="col">
+                      <div className="card h-100">
+                        <div className="card-body">
+                          <h5 className="card-title">{item.nombre}</h5>
                         </div>
-                        
-                    ))}
-                </div>
+                        <hr />
+                        <div className="card-footer">
+                          <h5 className="text-muted">$ {item.correo}</h5>
+                        </div>
+                        <div className="buttonHome">
+                          <button
+                            className="btn btn-primary"
+                            onClick={() => {
+                              navigate(`/Favoritos/${item.id}`);
+                            }}
+                          >
+                            Favoritos 👀
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
             </div>
-          <div class="col-6">Podrás incluir tus MyFood Favoritos, revisar tus ultimos pedidos y más. </div>
-
+          </div>
+          <div class="col-6">
+            Podrás incluir tus MyFood Favoritos, revisar tus ultimos pedidos y
+            más.
+          </div>
         </div>
-      </div></>
+      </div>
+    </>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useContext,  } from "react";
+// import { UserContext } from "../context/UserContext";
+// import { perfilContext } from "../context/PerfilContext";
+
+// export default function Perfil() {
+//   const { setUsuario } = useContext(UserContext);
+//   const { setUsuarios} = useContext(perfilContext);
+
+//   useEffect(() => {
+//     const usuarioLocalStorage = localStorage.getItem("usuario");
+//     if (usuarioLocalStorage) {
+//       // Si hay un usuario registrado en el localStorage, actualiza el contexto
+//       setUsuario(JSON.parse(usuarioLocalStorage));
+//     }
+//   }, []);
+
+
+//   return (
+//     <><div>
+//       <h1> <span className="badge bg-secondary">Bienvenid@</span></h1>
+//     </div><div class="container text-center">
+//         <div class="row">
+//         <div >
+//          {/* <Carousel/> */}
+//                 <div className="perfil" >
+//                     {usuarios.map(item =>(
+//                         <div className="row-cols-1 row-cols-md-1 " key={item.id} >
+//                             <div className="col">
+//                                 <div className="card h-100">                          
+//                                     <div className="card-body">
+//                                         <h5 className="card-title">{item.nombre}</h5>
+//                                     </div>
+//                                     <hr />
+//                                     <div className="card-footer" >
+//                                         <h5 className="text-muted">$ {item.correo}</h5>                              
+//                                     </div>
+//                                     <div className="buttonHome">
+//                                         <button className="btn btn-primary" onClick={() => {  
+//                                             navigate(`/Favoritos/${item.id}`);
+//                                         }}>Favoritos 👀</button>
+                                      
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         </div>
+                        
+//                     ))}
+//                 </div>
+//             </div>
+//           <div class="col-6">Podrás incluir tus MyFood Favoritos, revisar tus ultimos pedidos y más. </div>
+
+//         </div>
+//       </div></>
+//   );
+// }
